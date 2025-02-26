82ffe5c8dd74a5e98f42bfc5edacf6372a093f54
Karina <94112873+Voloshyna-Karina@users.noreply.github.com>
2025-02-26 18:49:41 +0200

    add hello page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import Hello from './components/Hello/Hello.jsx';
+// import Hello from './components/Hello/Hello.jsx';
+import DashboardLayoutBasic from "./components/DashboardLayoutBasic/DashboardLayoutBasic.jsx";
 
 function App() {
     return (
         <Routes>
-            <Route path="/" element={<div>Home Page</div>} />
-            <Route path="/user" element={<Hello/>} />
+            <Route path="/" element={<DashboardLayoutBasic/>} />
+            <Route path="/user" element={<DashboardLayoutBasic/>} />
         </Routes>
     );
 }
--- src/components/Hello/Hello.jsx
+++ src/components/Hello/Hello.jsx
@@ -1,7 +1,25 @@
+import {useEffect, useState} from "react";
+
 const Hello = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/api/users')
+            .then((res) => res.json())
+            .then((data) => setUsers(data))
+            .catch((err) => console.error(err));
+    }, []);
+
     return (
         <div>
-            <p style={{color: "red", fontSize: "20px"}}>hello world</p>
+            <div>
+                <h1>Users from Backend</h1>
+                <ul>
+                    {users.map((user) => (
+                        <li key={user.id}>{user.name}</li>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
